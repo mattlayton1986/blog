d898028a24f189823ea7c61cda479f2f86004512
Matt Layton <layton.matt1986@gmail.com>
2017-08-27 19:44:48 -0500

    Add support pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Neorxnawang"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
+
+# Todo:
+# Set appropriate base_title for production site
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'support_pages/home'
 
-  get 'support_pages/about'
-
-  get 'support_pages/contact'
+  root 'support_pages#home'
+  get '/about', to: 'support_pages#about'
+  get '/contact', to: 'support_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
